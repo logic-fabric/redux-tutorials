8b0277dedc527d2f9d118ddc6ffd325b46b55f36
logic-fabric <lowic@logic-fabric.fr>
2021-09-15 10:20:26 +0200

    refactor: /src files minor refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+
+import { Counter } from "./features/counter/Counter";
+
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <Counter />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <span>
           <span>Learn </span>
           <a
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import {
   decrement,
   increment,
@@ -7,13 +8,14 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice";
+
+import styles from "./Counter.module.css";
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   const incrementValue = Number(incrementAmount) || 0;
 
@@ -22,44 +24,50 @@ export function Counter() {
       <div className={styles.row}>
         <button
           className={styles.button}
-          aria-label="Decrement value"
+          aria-label="Décrémenter la valeur"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
+
         <span className={styles.value}>{count}</span>
+        
         <button
           className={styles.button}
-          aria-label="Increment value"
+          aria-label="Incrementer la valeur"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
       </div>
+
       <div className={styles.row}>
         <input
           className={styles.textbox}
-          aria-label="Set increment amount"
+          aria-label="Fixer le pas d'incrémentation"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
+
         <button
           className={styles.button}
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
-          Add Amount
+          Ajouter la valeur
         </button>
+
         <button
           className={styles.asyncButton}
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
-          Add Async
+          Ajouter en asynchrone
         </button>
+
         <button
           className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
         >
-          Add If Odd
+          Ajouter si impair
         </button>
       </div>
     </div>
