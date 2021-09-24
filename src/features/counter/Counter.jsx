b5e75cb9cd1ea1392a1a9d43ebc6160760115e85
logic-fabric <lowic@logic-fabric.fr>
2021-09-24 15:05:42 +0200

    feature: add incrementByStep button

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { decrement, increment } from "./counterSlice";
+import { decrement, increment, incrementByStep } from "./counterSlice";
 
 import "./Counter.css";
 
@@ -9,25 +9,44 @@ export function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
+  const [incrementStep, setIncrementStep] = useState("2");
+
   return (
-    <div className="counter-container">
-      <button
-        className="counter__btn"
-        aria-label="Incrémenter le compteur"
-        onClick={() => dispatch(increment())}
-      >
-        incrémenter
-      </button>
+    <div>
+      <div className="counter__row">
+        <button
+          className="counter__btn"
+          aria-label="Incrémenter le compteur"
+          onClick={() => dispatch(increment())}
+        >
+          incrémenter
+        </button>
 
-      <span className="counter__value">{count}</span>
+        <span className="counter__value">{count}</span>
 
-      <button
-        className="counter__btn"
-        aria-label="Décrémenter le compteur"
-        onClick={() => dispatch(decrement())}
-      >
-        décrémenter
-      </button>
+        <button
+          className="counter__btn"
+          aria-label="Décrémenter le compteur"
+          onClick={() => dispatch(decrement())}
+        >
+          décrémenter
+        </button>
+      </div>
+
+      <div className="counter__row">
+        <input
+          value={incrementStep}
+          label-aria="Fixer le pas d'incrémentation"
+          onChange={(e) => setIncrementStep(e.target.value)}
+        />
+
+        <button
+          aria-label="Incrémenter par pas"
+          onClick={() => dispatch(incrementByStep(+incrementStep || 0))}
+        >
+          incrémenter d'un pas
+        </button>
+      </div>
     </div>
   );
 }
